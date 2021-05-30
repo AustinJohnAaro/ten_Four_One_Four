b5790e2e55fbb34e0606ad87c05367397364d012
AustinJohnAaro <austin1472a@gmail.com>
2021-05-30 11:55:28 -0700

    do you see what I see

--- controller/index.js
+++ controller/index.js
@@ -8,4 +8,13 @@ router.use('/', homeRoutes);
 router.use('/dashboard', dashboardRoutes);
 router.use('/api', apiRoutes);
 
+// Pass true to env() to make it use the current environment stage.
+
+require('custom-env').env(true)
+
+console.log(process.env.APP_NAME)
+console.log(process.env.USERNAME)
+console.log(process.env.PASSKEY)
+NODE_ENV=staging 
+
 module.exports = router; 
